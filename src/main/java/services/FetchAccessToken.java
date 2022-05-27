77db6bc3ac86d3be587d064c908a29896b37b52f
RahulJadhwani1903 <92024230+RahulJadhwani1903@users.noreply.github.com>
2022-05-27 16:11:35 +0200

    Initial commit with project structure

---
+++ src/main/java/services/FetchAccessToken.java
@@ -0,0 +1,17 @@
+package services;
+
+import static io.restassured.RestAssured.*;
+
+public class FetchAccessToken {
+
+    //POST for fetching access token and logic to verify its validity
+
+    public String fetchAccessToken(){
+        given().get();
+
+
+        return "";
+    }
+
+
+}
