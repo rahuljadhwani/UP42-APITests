4683a3e9ef905225ddefcda17709149db4b6b21f
RahulJadhwani1903 <92024230+RahulJadhwani1903@users.noreply.github.com>
2022-05-30 11:51:05 +0200

    Added ITestListener implementation to provide details for failed tests in emailable-report.html

---
+++ src/main/java/listeners/TestListener.java
@@ -0,0 +1,45 @@
+package listeners;
+
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+import org.testng.Reporter;
+
+public class TestListener implements ITestListener {
+
+    @Override
+    public void onTestStart(ITestResult result) {
+
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        Reporter.log(result.getMethod().getMethodName()+" Passed");
+    }
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        Reporter.log(result.getMethod().getMethodName()+" Failed");
+        Reporter.log(result.getThrowable().toString());
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult result) {
+
+    }
+
+    @Override
+    public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+
+    }
+
+    @Override
+    public void onStart(ITestContext context) {
+
+    }
+
+    @Override
+    public void onFinish(ITestContext context) {
+
+    }
+}
