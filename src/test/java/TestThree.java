6202a779a35d382f9dcc40ca3b79b49cb9a35f13
RahulJadhwani1903 <92024230+RahulJadhwani1903@users.noreply.github.com>
2022-06-09 17:09:35 +0200

    Small test for Jenkins

---
+++ src/test/java/TestThree.java
@@ -0,0 +1,17 @@
+import constants.FrameworkConstants;
+import org.testng.annotations.Test;
+
+import java.util.Base64;
+
+public class TestThree {
+
+
+
+
+    @Test
+    public void testingEncodingDecoding(){
+        System.out.println(">>>>>>>>>>> Started  <<<<<<<<<<<<<<");
+    }
+
+
+}
