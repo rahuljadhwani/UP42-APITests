356b5c809abc2abd979a8cbd34673157887013e0
RahulJadhwani1903 <92024230+RahulJadhwani1903@users.noreply.github.com>
2022-06-09 17:35:09 +0200

    Small test for Jenkins

--- src/test/java/TestThree.java
+++ src/test/java/TestThree.java
@@ -6,11 +6,9 @@ import java.util.Base64;
 public class TestThree {
 
 
-
-
     @Test
-    public void testingEncodingDecoding(){
-           System.out.println(">>>>>>>>>>> Started  <<<<<<<<<<<<<<");
+    public void testingEncodingDecoding() {
+        System.out.println(">>>>>>>>>>> Started  <<<<<<<<<<<<<<");
     }
 
 
